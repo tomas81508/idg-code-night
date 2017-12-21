5b45b256af68ccb6748fa9d9815d2ffe34024396
Johan Lindeberg <jlindebe@kth.se>
2017-12-21 23:05:21 +0100

    prev button, state as a list of states

--- src/game_of_life/main.cljs
+++ src/game_of_life/main.cljs
@@ -13,17 +13,22 @@
                                  " ### "
                                  "  #  "))
 
-(defonce app-state-atom (atom initial-state))
+(defonce app-state-atom (atom (list initial-state)))
 
 (defn handle-event [{name :name data :data}]
   (cond (= name :next-generation)
-        (swap! app-state-atom next-generation)
+        (swap! app-state-atom (fn [state]
+                                (conj state (next-generation (first state)))))
 
         (= name :cell-click)
-        (swap! app-state-atom toggle-cell data)))
+        (swap! app-state-atom toggle-cell data)
+
+        (= name :prev-generation)
+        (when (> (count @app-state-atom) 1)
+          (swap! app-state-atom (fn [atom] (drop 1 atom))))))
 
 (defn render! []
-  (render-component [app-component {:state @app-state-atom
+  (render-component [app-component {:state         (first @app-state-atom)
                                     :trigger-event handle-event}]
                     (. js/document (getElementById "app"))))
 
